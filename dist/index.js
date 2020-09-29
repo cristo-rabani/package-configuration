a284ab7cb9435b7739f97fa30c11c15c9c700538
cristo <cristo.rabani@gmail.com>
2020-09-30 00:27:24 +0200

    actions

--- dist/index.js
+++ dist/index.js
@@ -1343,12 +1343,21 @@ const util = __webpack_require__(669);
 const readFileAsync = util.promisify(fs.readFile);
 
 async function run() {
+  const mainKey = core.getInput('mainKey');
   const keyName = core.getInput('keyName');
+  const keys = [];
+  if (mainKey) {
+    keys.push(mainKey);
+  }
+  if (keyName) {
+    keys.push(keyName);
+  }
+  const key = keys.join('.');
   try {
     const buffer = await readFileAsync('package.json');
     const json = JSON.parse(buffer.toString());
-    console.log('keys', keyName);
-    const targetConf = get(keyName, json);
+    console.log('key', key);
+    const targetConf = get(json, key);
     console.log('Configuration', targetConf);
     if (!targetConf) {
       core.setFailed('configuration not found :-(');
--- src/index.js
+++ src/index.js
@@ -5,12 +5,21 @@ const util = require('util');
 const readFileAsync = util.promisify(fs.readFile);
 
 async function run() {
+  const mainKey = core.getInput('mainKey');
   const keyName = core.getInput('keyName');
+  const keys = [];
+  if (mainKey) {
+    keys.push(mainKey);
+  }
+  if (keyName) {
+    keys.push(keyName);
+  }
+  const key = keys.join('.');
   try {
     const buffer = await readFileAsync('package.json');
     const json = JSON.parse(buffer.toString());
-    console.log('keys', keyName);
-    const targetConf = get(keyName, json);
+    console.log('key', key);
+    const targetConf = get(json, key);
     console.log('Configuration', targetConf);
     if (!targetConf) {
       core.setFailed('configuration not found :-(');
